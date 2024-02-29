24dae79c9d440645a11c293079a5706f92d1a865
marihab <marihabade@gmail.com>
2024-02-29 17:15:56 +0100

    Do express initial Set Up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+app.use(errorHandler)
+app.listen(process.env.PORT || 5000)
